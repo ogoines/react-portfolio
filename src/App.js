dcfb8d37d7eb15a571ece3f99328776a54533157
OGrunner <orshagoines@gmail.com>
2022-10-20 17:44:12 -0400

    styling

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import MainContainer from './pages/MainContainer';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+   <MainContainer />
+    </div>
+  );
+}
+
+export default App;
